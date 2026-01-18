032cdd96a8c223f4c7295d37cd38ecb3aaec5741
Tyler Campbell <tcampb30@gmail.com>
2026-01-18 16:54:49 -0500

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import CalendlyScheduler from "./CalendlyScheduler.tsx";
 
-// ecf8d175-5241-4d4e-81f5-5c23d15038b0&event_type_uuid=5accc827-edc9-45b9-9cf1-a6ed4f2c03f2
-const CLIENT_ID = "ecf8d175-5241-4d4e-81f5-5c23d15038b0"
-const EVENT_TYPE_UUID = "5accc827-edc9-45b9-9cf1-a6ed4f2c03f2"
+const CLIENT_ID = ""
+const EVENT_TYPE_UUID = ""
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CalendlyScheduler
       clientId={CLIENT_ID}
       eventTypeUuid={EVENT_TYPE_UUID}
-      availabilityOnly={true}
+      availabilityOnly={false}
     />
   </StrictMode>,
 );
